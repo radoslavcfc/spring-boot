cd6c1d6847560a6079bc74483037b5e7f68ad941
Radoslav Hadzhiiliev <radoslav.hadzhiiliev@ivcevidensia.com>
2026-08-14 14:49:59 +0300

    feat: refactor CashCardController to use repository for data retrieval and enhance test logging

--- spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
+++ spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
@@ -6,27 +6,35 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        if (requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
+//        if (requestedId.equals(99L)) {
+//            CashCard cashCard = new CashCard(99L, 123.45);
+//            return ResponseEntity.ok(cashCard);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
-    }
 
-//      @GetMapping("/{requestedId}")
-// private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-//     if (requestedId.equals(99L)) {
-//         CashCard cashCard = new CashCard(99L, 123.45);
-//         return ResponseEntity.ok(cashCard);
-//     } else {
-//         return ResponseEntity.notFound().build();
-//     }
-// }
+// Suggested:
+//        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+//        return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 }
