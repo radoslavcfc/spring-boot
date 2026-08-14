20f4d3264acd269770e1339188177939ed8ce2d4
Radoslav Hadzhiiliev <radoslav.hadzhiiliev@ivcevidensia.com>
2026-08-14 14:16:31 +0300

    feat: add CashCard repository and update controller for improved data handling

--- spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
+++ spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
@@ -12,10 +12,12 @@ class CashCardController {
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-
-        CashCard cashCard = new CashCard(99L, 123.45);
-
-        return ResponseEntity.ok(cashCard);
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 //      @GetMapping("/{requestedId}")
