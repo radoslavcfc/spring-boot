6a800a2970d1f8d9f706bbabc2a85b43fb92211e
Radoslav Hadzhiiliev <radoslav.hadzhiiliev@ivcevidensia.com>
2026-08-17 16:29:31 +0300

    feat: enhance CashCard creation endpoint to return location URI and validate response

--- spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
+++ spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
@@ -2,7 +2,9 @@ package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -36,7 +38,12 @@ class CashCardController {
     }
 
     @PostMapping
-    private ResponseEntity<Void> createCashCard() {
-        return null;
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+            .path("cashcards/{id}")
+            .buildAndExpand(savedCashCard.id())
+            .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
