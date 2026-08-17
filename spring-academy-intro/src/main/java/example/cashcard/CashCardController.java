c99c586d2ef480051c769629f30b0db0f50f25b5
Radoslav Hadzhiiliev <radoslav.hadzhiiliev@ivcevidensia.com>
2026-08-17 16:06:37 +0300

    feat: add CashCard creation endpoint and corresponding test

--- spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
+++ spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
@@ -1,10 +1,7 @@
 package example.cashcard;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -36,5 +33,10 @@ class CashCardController {
 // Suggested:
 //        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
 //        return cashCardOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard() {
+        return null;
     }
 }
--- spring-academy-intro/src/test/java/example/cashcard/CashCardApplicationTests.java
+++ spring-academy-intro/src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebCl
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -37,5 +39,15 @@ class CashCardApplicationTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
+    void shouldCreateANewCashCard() {
+        CashCard newCashCard = new CashCard(null, 250.00);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+        ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+        assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
