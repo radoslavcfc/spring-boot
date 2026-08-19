dc1db09a616df7ace205f6a76f95d20fb629fd40
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2026-08-19 09:42:34 +0300

    feat: enhance test for retrieving all CashCards to validate response structure and content

--- spring-academy-intro/src/test/java/example/cashcard/CashCardApplicationTests.java
+++ spring-academy-intro/src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -2,6 +2,7 @@ package example.cashcard;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -60,12 +61,19 @@ class CashCardApplicationTests {
         assertThat(amount).isEqualTo(250.00);
     }
 
-
     @Test
     void shouldReturnAllCashCardsWhenListIsRequested() {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-    }
 
-    //4: Enhance the List Test
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        int cashCardCount = documentContext.read("$.length()");
+        assertThat(cashCardCount).isEqualTo(3);
+
+        JSONArray ids = documentContext.read("$..id");
+        assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+
+        JSONArray amounts = documentContext.read("$..amount");
+        assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.0, 150.00);
+    }
 }
