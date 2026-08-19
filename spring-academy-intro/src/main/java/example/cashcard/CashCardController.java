8591cebbf9359d321878338feee2bdd61e9deaee
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2026-08-19 11:06:14 +0300

    feat: add sorting functionality to CashCard retrieval endpoint and corresponding tests

--- spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
+++ spring-academy-intro/src/main/java/example/cashcard/CashCardController.java
@@ -3,6 +3,7 @@ package example.cashcard;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -56,7 +57,8 @@ class CashCardController {
         Page<CashCard> page = cashCardRepository.findAll(
             PageRequest.of(
                 pageable.getPageNumber(),
-                pageable.getPageSize()
+                pageable.getPageSize(),
+                pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
             ));
         return ResponseEntity.ok(page.getContent());
     }
