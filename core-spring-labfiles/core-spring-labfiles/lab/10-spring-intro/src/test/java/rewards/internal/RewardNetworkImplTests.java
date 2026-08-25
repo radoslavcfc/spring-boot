506de4cc08faffbf333c32b52f1321d7879c1a02
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2026-08-25 15:21:48 +0300

    enable the test

--- core-spring-labfiles/core-spring-labfiles/lab/10-spring-intro/src/test/java/rewards/internal/RewardNetworkImplTests.java
+++ core-spring-labfiles/core-spring-labfiles/lab/10-spring-intro/src/test/java/rewards/internal/RewardNetworkImplTests.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * Configures the implementation with stub repositories
  * containing dummy data for fast in-memory testing without
  * the overhead of an external data source.
- * 
+ *
  * Besides helping catch bugs early, tests are a great way
  * for a new developer to learn an API as he or she can see the
  * API in action. Tests also help validate a design as they
